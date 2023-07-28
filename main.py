3cada76fda0688485b5be1fd2b40d711dddd3701
Ali Haghighat <116483401+AliHaghighat1@users.noreply.github.com>
2023-07-28 01:43:10 -0600

    Update main.py

--- main.py
+++ main.py
@@ -324,7 +324,7 @@ fig4a.update_layout(title='Trend in complaints received over time',
                    xaxis_title='Date Received',
                    yaxis_title='Number of Complaints')
 
-fig4a.show()
+#fig4a.show()
 
 
 
